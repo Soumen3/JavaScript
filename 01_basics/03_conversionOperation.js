3bce73aaf73b77946c65dd28cc226029d7882f5d
Soumen Samanta <111648383+Soumen3@users.noreply.github.com>
2023-11-29 14:51:47 +0000

    convrsion of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,35 @@
+let score=undefined
+
+console.log(typeof score);
+
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+/*
+"33" => 33
+"33ag" => NaN
+true => 1
+false => 0
+*/
+
+
+let isLoggedin="af"
+let booleanIsLoggedIn=Boolean(isLoggedin);
+console.log(booleanIsLoggedIn);
+
+/*
+1 => true
+0 => false      
+85 => true      any value that is not 0 or empty string will convert to true
+"" => false
+"fjn" => true
+*/
+
+
+let someNumber=47
+
+let stringNumver=String(someNumber)
+console.log(stringNumver)
+console.log(typeof stringNumver);
